e25dfd7e4f93d9f8a6f8b3c516cd33d6a02a5c99
Jen <38834893+aides9@users.noreply.github.com>
2021-04-21 15:45:25 +0800

    Update Tags.cs

--- SeoAnalyzer.Business/Constants/Tags.cs
+++ SeoAnalyzer.Business/Constants/Tags.cs
@@ -3,9 +3,9 @@ namespace SeoAnalyzer.Business.Constants
 {
     public static class Tags
     {
-        public static readonly string Script = "script";
-        public static readonly string Style = "style";
-        public static readonly string Content = "content";
-        public static readonly string Href = "href";
+        public const string Script = "script";
+        public const string Style = "style";
+        public const string Content = "content";
+        public const string Href = "href";
     }
 }
